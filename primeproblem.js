ee8cb4fd8e1d15aef70f65dac2c1ebe011e30e91
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2023-02-08 03:01:34 +0530

    Create primeproblem.js

---
+++ primeproblem.js
@@ -0,0 +1,16 @@
+let num = 11;
+
+let count = 0;
+
+for(let i=0; i<num; i++){
+
+    if(i % num == 1){
+        count= 1
+    }
+
+}
+if(count == 1){
+    console.log("yes it is prime")
+}else{
+    console.log("not a palidrome")
+}
